14b53ae840c9086f8145beb8d69f90ec676367cf
Jeferson Mesa <jefersondmj21@gmail.com>
2022-11-14 19:26:56 -0500

    ManejoformulariosHTML added

---
+++ ManejoFormulariosHTML/src/main/webapp/recursos/funciones.js
@@ -0,0 +1,57 @@
+function validarForm(forma){
+    var usuario = forma.usuario;
+    if(usuario.value == "" || usuario.value == "Escribir usuario"){
+        alert("Debe proporcionar un nombre de usuario");
+        usuario.focus();
+        usuario.select();
+        return false;
+    }
+    
+    var password = forma.password;
+    if(password.value == "" || password.value.length < 3){
+        alert("Debe proporcionar un password de al menos 3 caracteres");
+        password.focus();
+        password.select();
+        return false;
+    }
+    
+    var tecnologias = forma.tecnologia;
+    var checkSeleccionado = false;
+    
+    for(var i = 0; i < tecnologias.length; i++){
+        if(tecnologias[i].checked){
+            checkSeleccionado = true;
+        }
+    }
+    
+    if(!checkSeleccionado){
+        alert("Debe seleccionar alguna tecnologia");
+        return false;
+    }
+    
+    var generos = forma.genero;
+    var radioSeleccionado = false;
+    
+    for(var i = 0; i < generos.length; i++){
+        if(generos[i].checked){
+            radioSeleccionado = true;
+        }
+    }
+    
+    if(!radioSeleccionado){
+        alert("Debe elegir un genero");
+        return false;
+    }
+    
+    var ocupacion = forma.ocupacion;
+    if(ocupacion.value == ""){
+        alert("Debe proporcionar una ocupacion");
+        return false;
+    }
+    
+    //Formulario es valido
+    alert("Formulario es valido, enviando datos al servidor");
+    return true;
+}
+
+
